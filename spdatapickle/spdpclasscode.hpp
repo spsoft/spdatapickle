1c31d3e00f4712004c3884dfa53807d332751e31
stephen.nil <stephen.nil@fff870a6-e63e-11dd-9ba5-89a75009fd5d>
2010-12-13 12:58:13 +0000

    add spxml2class tools

---
+++ spdatapickle/spdpclasscode.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2010 Stephen Liu
+ * For license terms, see the file COPYING along with this library.
+ */
+
+#pragma once
+
+#include <stdio.h>
+
+class SP_DPSyntaxTree;
+class SP_DPSyntaxStruct;
+class SP_DPSyntaxField;
+
+class SP_DPNameRender;
+
+class SP_DPClassCodeRender
+{
+public:
+	SP_DPClassCodeRender( SP_DPNameRender *nameRender );
+	~SP_DPClassCodeRender();
+
+	void generateHeader( SP_DPSyntaxTree * syntaxTree, FILE * writer );
+
+	void generateCpp( SP_DPSyntaxTree * syntaxTree, FILE * writer );
+
+private:
+	void generateDecl( SP_DPSyntaxStruct * structure, FILE * writer );
+
+	void generateAccessorDecl( SP_DPSyntaxField * field, FILE * writer );
+
+	void generateImpl( const char * pickleName,
+			SP_DPSyntaxStruct * structure, FILE * writer );
+
+	void generateEval( const char * pickleName,
+			SP_DPSyntaxStruct * structure, FILE * writer );
+
+	void generateAccessorImpl( const char * className,
+			SP_DPSyntaxField * field, FILE * writer );
+
+private:
+	SP_DPNameRender * mNameRender;
+};
+
